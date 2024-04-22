5d8160412b2ef1e8cdd4e3b8131e926deacf7be2
Sudharshan S <79196955+sudharshans2009@users.noreply.github.com>
2024-04-22 16:47:54 +0530

    Bug Fix

--- src/config/collaborations.js
+++ src/config/collaborations.js
@@ -2,19 +2,19 @@ import Link from "next/link";
 
 export const collaborations = [
   {
-    title: <>Impacts of recent El-Niño Southern Oscillation (ENSO) on the Water-Food-Energy Nexus in South Asia (<Link className="border-b border-amrita hover:border-white transition-all border-dashed" href="https://amrita.edu/research/project/impacts-recent-el-nino-southern-oscillation-enso-water-food-energy-nexus-south-asia)">https://amrita.edu/research/project/impacts-recent-el-nino-southern-oscillation-enso-water-food-energy-nexus-south-asia)</Link></>,
+    title: <>Impacts of recent El-Niño Southern Oscillation (ENSO) on the Water-Food-Energy Nexus in South Asia (<Link className="border-b border-amrita hover:border-white transition-all border-dashed" href="https://amrita.edu/research/project/impacts-recent-el-nino-southern-oscillation-enso-water-food-energy-nexus-south-asia)">https://amrita.edu/research/project/impacts-recent-el-nino-southern-oscillation-enso-water-food-energy-nexus-south-asia</Link>)</>,
     institutions: [<>Uppsala University, Sweden</>, <>Indian Institute of technology Roorkee</>]
   },
   {
-    title: <>Water and Energy Saving Reliable Irrigation System (WatEr-ERIS): Solar Energy and Cloud-based Decision Support System for Automated Irrigation System (<Link className="border-b border-amrita hover:border-white transition-all border-dashed" href="https://amrita.edu/research/project/water-and-energy-saving-reliable-irrigation-system-water-eris-solar-energy-and)">https://amrita.edu/research/project/water-and-energy-saving-reliable-irrigation-system-water-eris-solar-energy-and)</Link></>,
+    title: <>Water and Energy Saving Reliable Irrigation System (WatEr-ERIS): Solar Energy and Cloud-based Decision Support System for Automated Irrigation System (<Link className="border-b border-amrita hover:border-white transition-all border-dashed" href="https://amrita.edu/research/project/water-and-energy-saving-reliable-irrigation-system-water-eris-solar-energy-and)">https://amrita.edu/research/project/water-and-energy-saving-reliable-irrigation-system-water-eris-solar-energy-and</Link>)</>,
     institutions: [<>Indian Institute of technology Roorkee</>, <>Indian Institute of technology Mandi</>]
   },
   {
-    title: <>Structured Dialogues for Sustainable Urban Water Management (<Link className="border-b border-amrita hover:border-white transition-all border-dashed" href="https://amrita.edu/research/project/structured-dialogues-sustainable-urban-water-management-copi)">https://amrita.edu/research/project/structured-dialogues-sustainable-urban-water-management-copi)</Link></>,
+    title: <>Structured Dialogues for Sustainable Urban Water Management (<Link className="border-b border-amrita hover:border-white transition-all border-dashed" href="https://amrita.edu/research/project/structured-dialogues-sustainable-urban-water-management-copi)">https://amrita.edu/research/project/structured-dialogues-sustainable-urban-water-management-copi</Link>)</>,
     institutions: [<>Birla Institute of Technology & Science, Pilani</>, <>Birla Institute of Technology & Science, Hyderabad</>, <>Aligarh Muslim University</>]
   },
   {
-    title: <>Mitigation of dam induced flood disaster due to hydrological extremes (<Link className="border-b border-amrita hover:border-white transition-all border-dashed" href="https://amrita.edu/research/project/mitigation-dam-induced-flood-disaster-due-hydrological-extremes-copi)">https://amrita.edu/research/project/mitigation-dam-induced-flood-disaster-due-hydrological-extremes-copi)</Link></>,
+    title: <>Mitigation of dam induced flood disaster due to hydrological extremes (<Link className="border-b border-amrita hover:border-white transition-all border-dashed" href="https://amrita.edu/research/project/mitigation-dam-induced-flood-disaster-due-hydrological-extremes-copi)">https://amrita.edu/research/project/mitigation-dam-induced-flood-disaster-due-hydrological-extremes-copi</Link>)</>,
     institutions: [<>Heriot-Watt University, UK</>, <>University of Calgary, Canada</>, <>Indian Institute of technology Roorkee</>]
   },
   {
@@ -22,7 +22,7 @@ export const collaborations = [
     institutions: [<>Indian Institute of technology Bhubaneshwar</>, <>Indian Institute of technology Palakkad</>]
   },
   {
-    title: <>Understanding Impacts of El-Nino events on the Indian Agricultural Productivity (<Link className="border-b border-amrita hover:border-white transition-all border-dashed" href="https://amrita.edu/research/project/understanding-impacts-el-nino-events-indian-agricultural-productivity-copi)">https://amrita.edu/research/project/understanding-impacts-el-nino-events-indian-agricultural-productivity-copi)</Link></>,
+    title: <>Understanding Impacts of El-Nino events on the Indian Agricultural Productivity (<Link className="border-b border-amrita hover:border-white transition-all border-dashed" href="https://amrita.edu/research/project/understanding-impacts-el-nino-events-indian-agricultural-productivity-copi)">https://amrita.edu/research/project/understanding-impacts-el-nino-events-indian-agricultural-productivity-copi</Link>)</>,
     institutions: [<>Heriot-Watt University, UK</>, <>Indian Institute of technology Kharagpur</>]
   }
 ];
