28b9832fcd83becb4075319740b19d4119475345
Sudharshan S <79196955+sudharshans2009@users.noreply.github.com>
2024-04-22 16:41:39 +0530

    Bug Fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Copyright, GraduationCap, Linkedin, Twitter } from "lucide-react";
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
   return (
