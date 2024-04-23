8cc39328f6c48589b89e854a4f299d7e03c8f845
sudharshans2009 <sudharshans2009@gmail.com>
2024-04-23 20:40:08 +0530

    Bug Fix

--- src/app/notion/route.js
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function GET() {
-  return NextResponse.redirect("https://sudharshans2009.notion.site/bsoundharajan-vercel-app-e7a891dcf0764390af66b1a26e77fcfb?pvs=4");
-}
