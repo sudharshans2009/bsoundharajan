91074f668327dc57243ed840b5e8c87e5617f399
sudharshans2009 <sudharshans2009@gmail.com>
2024-12-21 21:52:49 +0530

    Bug Fixes

--- src/config/main.js
+++ src/config/main.js
@@ -24,3 +24,54 @@ export const nav = [
     link: "/collaborations"
   }
 ];
+
+export const experience = [
+  {
+    time: <>Since July 2019</>,
+    position: <>Associate Professor</>,
+    university: <>maincolor Vishwa Vidyapeetham, India</>
+  },
+  {
+    time: <>January 2018 - June 2019</>,
+    position: <>Assistant Professor (SG)</>,
+    university: <>maincolor Vishwa Vidyapeetham, India</>
+  },
+  {
+    time: <>August 2016 - January 2018</>,
+    position: <>Assistant Professor</>,
+    university: <>Arba Minch University, Ethiopia</>
+  },
+  {
+    time: <>April 2012 - June 2016</>,
+    position: <>Post-Doctoral Researcher</>,
+    university: <>Heriot-Watt University, UK</>
+  },
+  {
+    time: <>September 2014 - October 2014</>,
+    position: <>Adjunct Faculty</>,
+    university: <>Washington State University, USA</>
+  },
+  {
+    time: <>September 2011 - March 2012</>,
+    position: <>Scientist</>,
+    university: <>International Water Management Institute, India</>
+  }
+];
+
+export const education = [
+  {
+    education: <>PhD</>,
+    university: <>Indian Institute of Technology Madras</>,
+    time: <>2011</>
+  },
+  {
+    education: <>ME</>,
+    university: <>College of Engineering, Guindy, Anna University</>,
+    time: <>2004</>
+  },
+  {
+    education: <>BE</>,
+    university: <>Tamil Nadu Agricultural University</>,
+    time: <>2000</>
+  }
+]
